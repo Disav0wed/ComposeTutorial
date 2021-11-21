c5a3ba82e2e9321a8df8f003c37bd9638900a5bb
Ibrahim R Serpici <ibrahimrecepserpici@gmail.com>
2021-11-21 18:45:49 +0300

    compose version of the project

--- app/src/main/java/com/irs/composetutorial/LoginActivity.kt
+++ app/src/main/java/com/irs/composetutorial/LoginActivity.kt
@@ -1,41 +1,67 @@
 package com.irs.composetutorial
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.core.widget.addTextChangedListener
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.Dp
 
 class LoginActivity: AppCompatActivity() {
 
-    lateinit var userNameET : EditText
-    lateinit var loginBtn : AppCompatButton
-    private  var userName: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-        userNameET = findViewById(R.id.etUserName)
-        loginBtn = findViewById(R.id.btnLogin)
-
-        // Setting click listener for login
-        loginBtn.setOnClickListener {
-            if(userName.length >= 3){
-
-                var mainActivityIntent = Intent(this,MainActivity::class.java)
-                mainActivityIntent.putExtra("username",userName)
-                startActivity(mainActivityIntent)
-            }else{
-                Toast.makeText(this,"Username must be minimum 3 characters long!",Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        // Setting edittext to listen text changes
-        userNameET.addTextChangedListener {
-            userName = it.toString()
+        setContent { 
+            LoginCard(context = this)
         }
     }
+}
+
+@Composable
+fun LoginCard(context: Context){
+
+    var userName by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        // We are using column to align our small UI elements.
+        modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .wrapContentSize(align = Alignment.Center)){
+        TextField(
+            value = userName,
+            onValueChange = {
+                userName = it
+            },
+            placeholder = { Text(text = "Enter user name") },
+        )
+        Button(onClick = {
+            if(userName.length >= 3){
+
+                var mainActivityIntent = Intent(context,MainActivity::class.java)
+                mainActivityIntent.putExtra("username",userName)
+                context.startActivity(mainActivityIntent)
+            }else{
+                Toast.makeText(context,"Username must be minimum 3 characters long!",Toast.LENGTH_SHORT).show()
+            }
+        }, modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+            .padding(top = Dp(25F))) {
+            Text("Login")
+        }
+    }
+
 }
--- app/src/main/java/com/irs/composetutorial/MainActivity.kt
+++ app/src/main/java/com/irs/composetutorial/MainActivity.kt
@@ -2,22 +2,39 @@ package com.irs.composetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.AppCompatTextView
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var loggedInUserTV : AppCompatTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        loggedInUserTV = findViewById(R.id.tvLoggedInUser)
 
         // Get username from intent
         val userName : String = intent.extras?.get("username") as String
-
-        loggedInUserTV.text = "Welcome $userName"
-
+        setContent {
+            UserNameCard(userName)
+        }
     }
 }
+
+@Composable
+fun UserNameCard(userName: String){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .wrapContentSize(align = Alignment.Center)){
+        Text(text = "Welcome $userName")
+    }
+
+}
+
+
